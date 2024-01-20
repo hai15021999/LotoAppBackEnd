263384fdc58b71afb3658be8d5e42f20cca5f249
Casber <61676624+hai15021999@users.noreply.github.com>
2024-01-20 17:41:00 +0700

    update cors

--- utils/socket-io/socket-io.mjs
+++ utils/socket-io/socket-io.mjs
@@ -8,6 +8,10 @@ let SocketClients = null;
 const createSocketServer = (server, transports = ["websocket", "polling"]) => {
   SocketServer = new Server(server, {
     transports: transports,
+    cors: {
+      origin: ["https://bingo-loto.vercel.app/", "http://localhost"],
+      credentials: false,
+    },
   });
   SocketClients = new Map();
 };
